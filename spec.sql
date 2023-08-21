87bbf3ebd37dbd59c794ca85f60afe17e7b9f226
jacobglanz <jacobglanz9@gmail.com>
2023-08-21 13:47:42 -0400

    Clarified the spec

--- spec.sql
+++ spec.sql
@@ -1,13 +1,20 @@
 /*
 I do the following services for People who have a second home and they are not there a lot of times
-     1) Check their Mail
-     2) Clean their House
-     3) Monitor (Check for break ins and locks)
-I employ residents from the area to do the work, they get $0.25 for each mile they travel, and I always put a 100% markup on the total money that the employee gets, 
-but the markup is capped at $10. And I’ve had success, 
+     1) Mail, Check their Mail
+     2) Cleaning, Clean their House
+     3) Monitoring, Check for break ins and locks
+I employ residents from a area to do the jobs in that area, and they get paid $0.25 for each mile they travel to a destination.
+And i make my money by charging an additional fee -called Referral Fee- on every job, which is the same as the employee made for the job, but i never charge the customer more then $10 
+     So the internal business language for that is, "I put a 100% markup but it is capped at $10"
 
-When a customer calls me I need their Name, Email, Address to monitor, CC to charge.
-It doesn't really matter to me what other columns you add to you table, i guess just add the stuff you see fit in order to give me the necessary data i need
+Example story 1: Joseph wants to get his mail checked, i send Anna to do it, Anna travel 50 miles from whereever she is to Joseph's house,
+     i will pay anna $7.50 (30 miles traveled * $0.25 for each mile), and i will also charge a $7.50 Referral Fee
+Example story 2: Johnny  wants to get his mail checked, i send Amber to do it, Amber travel 50 miles from whereever she is to Johnny's house
+     i will pay Amber $12.50 (50 miles traveled * $0.25 for each mile), but my Referral Fee will only be $10.00 because it's a company policy
+     that the Referral Fee cannot be more the $10
+
+When a customer calls me I need their Name and address
+—----------------------
 
 The reports I need are as following:
      1) What is my markup % for each Job sort low to high, (I wanna see if it's sometimes too low and I should increase the cap.)
@@ -42,13 +49,27 @@ Answer: 1 Minutes
 
 Question: Would you go to the same house twice a day?
 Answer: Only to do a service that we haven't done on this house on this day already
+—----------------------
 
+Sample data:
+     Name, Address, DestinationWidth, DestinationHeight, NumServicesRequested, ServicesTime, EmployyeName, EmployeeEarnedMoney, ReferralFee
+     James, 123 Main St., 3, 9, 1, 2023-08-15 3:00 PM, Emily, null, null
+     Michael, 896 Main St., 8, 3, 1, 2023-08-15 4:00 PM, Emily, 2.75, 2.75
+     William, 235 First Ave., 88, 3, 2, 2023-08-15 5:00 PM, Emily, 40.0, 10
+
+
+
+Example story 1: Joseph wants to get his mail checked, i send Anna to do it, Anna travel 50 miles from whereever she is to Joseph's house,
+     i will pay anna $7.50 (30 miles traveled * $0.25 for each mile), and i will also charge a $7.50 Referral Fee
+Example story 2: Johnny  wants to get his mail checked, i send Amber to do it, Amber travel 50 miles from whereever she is to Johnny's house
+     i will pay Amber $12.50 (50 miles traveled * $0.25 for each mile), but my Referral Fee will only be $10.00 because it's a company policy
+     that the Referral Fee cannot be more the $10
 —----------------------
 
 See below an illustration on how to calculate distance between houses.
-In this scenario we are going from House A located on 3-9 to house B located at 8-3, we have to move 11 boxes (miles) to get from House A to Hous B.
+In this scenario we are going from House A located on 3-9 (3 on width and 9 on Height) to house B located at 8-3, we have to move 11 boxes (miles) to get from House A to Hous B.
 The customer on house B wants to check their mail.
-So in this case the employee travels 11 miles to do 1 service, he/she makes $8.25 and the company puts %100 markup, so the customer pays $16.50
+So in this case the employee travels 11 miles to do 1 service, he/she makes $2.75 and the company Referral Fee is $2.75 (%100 markup)
 
      1   2   3   4   5   6   7   8   9   10
 1  |___|___|___|___|___|___|___|___|___|___|
